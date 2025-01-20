f60888dd39874d91a8298830614e7963df9b6c2d
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-01-20 10:22:30 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -6435,19 +6435,3 @@ Tabs.Misc:AddButton({
         end
     end
 })
-Tabs.Misc:AddButton({
-    Title = "No Magma",
-    Description = "",    
-    Callback = function()
-       for i,v in pairs(game.Workspace:GetDescendants()) do
-           if v.Name == "Lava" then   
-               v:Destroy()
-           end
-       end
-       for i,v in pairs(game.ReplicatedStorage:GetDescendants()) do
-           if v.Name == "Lava" then   
-               v:Destroy()
-           end
-       end
-   end,
-})
