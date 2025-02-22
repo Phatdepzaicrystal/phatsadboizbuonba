d6dc3705c15d143a9b5d3c792285c4bb11ec0d31
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-02-22 09:41:57 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2604,31 +2604,6 @@ function to(v233)
         end
     end);
 end
--------------------------------------Auto Join----------------------------
-if not game:IsLoaded() then game.Loaded:Wait() end
-local fask = task
-
-if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
-    repeat task.wait()
-        if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
-            if getgenv().Team == "Marines" then
-                for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Marines"].Frame.TextButton.Activated)) do
-                    for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
-                        b:Fire() 
-                    end
-                    v.Function()
-                end 
-            else
-                for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Pirates"].Frame.TextButton.Activated)) do
-                    for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
-                        b:Fire() 
-                    end
-                    v.Function()
-                end 
-            end
-        end
-    until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
-end
 ----------------------------------Tab Sever-------------------------
 local Status = Tabs.Sever:AddSection("Sever Discord")
 
