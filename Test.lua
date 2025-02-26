6b10199c321e62434b84f61e35504bcd076f04f8
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-02-26 12:10:13 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,6 +1,6 @@
 --[[
 getgenv().Team = "Marines"          -- Pirates or Marines
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Phatdepzaicrystal/phatyeuem/refs/heads/main/Test.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Phatdepzaicrystal/Broken-Heart/refs/heads/main/Test.lua"))()
 ]] --
 if getgenv().Team == "Pirates" then
     game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("SetTeam", "Pirates")
