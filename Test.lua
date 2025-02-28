e425935459e2a4ed5e18aff3e40c92cb452b297d
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-02-28 21:05:27 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -39,7 +39,7 @@ local Tabs = {
     Player = Window:AddTab({Title = "PvP", Icon = "shield"}),
     Item = Window:AddTab({Title = "Get Item", Icon = "sword"}),
     Fish = Window:AddTab({Title = "Sea Event", Icon = "anchor"}),
-    Vocanic = Window:AddTab({Title = "Vocano Event", Icon = "mountain"}),	
+    Vocanic = Window:AddTab({Title = "Vocano Event", Icon = "rock"}),	
     Teleport = Window:AddTab({Title = "Teleport", Icon = "map"}),
     Fruit = Window:AddTab({Title = "Fruit", Icon = "apple"}),
     Raid = Window:AddTab({Title = "Raid", Icon = "target"}),
