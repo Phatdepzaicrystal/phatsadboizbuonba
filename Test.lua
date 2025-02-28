305cba34878273f8fb2280784d1b9e9b9330ff7e
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-02-28 20:28:33 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -45,7 +45,6 @@ local Tabs = {
     Race = Window:AddTab({Title = "Race", Icon = "flag"}),
     Shop = Window:AddTab({Title = "Shop", Icon = "shopping-bag"}),
     Misc = Window:AddTab({Title = "Misc", Icon = "menu"})
-    Vocanic = Window:AddTab({Title = "Vocanic Event", Icon = "fire"})
 }
 local Options = Fluent.Options
 do
