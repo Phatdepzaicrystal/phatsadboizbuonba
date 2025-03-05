04203c3036910142a113821000f93f8453e853f8
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-05 19:48:47 +0700

    Update Test.lua

--- Main/Test.lua
+++ Main/Test.lua
@@ -9916,46 +9916,41 @@ spawn(function()
 	end
 end);
 
-local StopTween1 = Tabs.Item:AddToggle("StopTween1", {
-    Title = "Stop Tween When Have Item",
-    Default = false
-})
-
-StopTween1:OnChanged(function(Value)
-    getgenv().StopChest = Value
-end)
-
-spawn(function()
-    while wait(0.5) do
-        if getgenv().StopChest then
-            local player = game.Players.LocalPlayer
-            if not player then continue end
-            
-            local backpack = player:FindFirstChild("Backpack")
-            local character = player.Character
-            
-            if backpack and character then
-                local itemsToCheck = {"Fist of Darkness", "God's Chalice"}
-                local foundItem = false
-                
-                for _, itemName in pairs(itemsToCheck) do
-                    if backpack:FindFirstChild(itemName) or character:FindFirstChild(itemName) then
-                        foundItem = true
+local StopTween1 = Tabs.Item:AddToggle("StopTween1", {Title = "Stop Tween When Have Item", Default = false})
+StopTween1:OnChanged(
+    function(Value)
+        getgenv().StopChest = Value
+    end
+)
+spawn(
+    function()
+        while wait(0.5) do
+            if getgenv().StopChest then
+                local player = game.Players.LocalPlayer
+                if not player then
+                    wait(0.5)
+                    return
+                end
+                local backpack = player:FindFirstChild("Backpack")
+                local character = player.Character
+                if backpack and character then
+                    if
+                        backpack:FindFirstChild("Fist of Darkness") or character:FindFirstChild("Fist of Darkness") or
+                            backpack:FindFirstChild("God's Chalice") or
+                            character:FindFirstChild("God's Chalice")
+                     then
+                        _G.AutoCollectChest = false
+                        if ToggleFarmChest and typeof(ToggleFarmChest.Set) == "function" then
+                            ToggleFarmChest:Set(false)
+                        end
                         break
                     end
-                end
-                
-                if foundItem then
-                    _G.AutoCollectChest = false
-                    if ToggleFarmChest and typeof(ToggleFarmChest.Set) == "function" then
-                        ToggleFarmChest:Set(false)
-                    end
-                    break
                 end
             end
         end
     end
-end)
+)
+
 -------------------------------------------------Tab Volcano----------------------------------------------------------------------------------
 local PlayersService = game:GetService("Players")
 local RunService = game:GetService("RunService")
